64252bbdf5fd4a2eab59a17160365f95880ad64c
Artur <artur.alimgafarov@gmail.com>
2022-07-28 14:03:15 +0500

    Class TimeSeries

---
+++ prediction_models.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+from sklearn import linear_model
+
+# INPUT: data - array of nums, k - count of predictions
+# OUTPUT: array of k predicted values
+
+class TimeSeries:
+    def __init__(self, data, k):
+        self.data = data
+        self.predictCount = k
+
+    def addModel(self):
+        pass
+
+    def multModel(self):
+        pass
+
+    def dvModel(self):
+        pass
