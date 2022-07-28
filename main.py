440910cc3628c4b6a6d877b6e5670aaa0ad9f0a9
Artur <artur.alimgafarov@gmail.com>
2022-07-28 15:31:40 +0500

    Displayed charts with 3 model's predict values on plot

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ def main():
 
     timeSeries = ts.TimeSeries(data, predictionCount)
 
-    am = data[-1] + timeSeries.addModel()['predict']
-    mm = data[-1] + timeSeries.multModel()['predict']
-    dv = data[-1] + timeSeries.dvModel()['predict']
+    am = [data[-1]] + list(timeSeries.addModel()['predict'])
+    mm = [data[-1]] + list(timeSeries.multModel()['predict'])
+    dv = [data[-1]] + list(timeSeries.dvModel()['predict'])
 
     n = len(data)
     dataTime = list(range(1, n + 1))
     predictTime = list(range(n, n + 1 + predictionCount))
 
-    print(dataTime)
-    print(predictTime)
+    plt.plot(dataTime, data, '-b')
+    plt.plot(predictTime, am, '--r')
+    plt.plot(predictTime, mm, '--g')
+    plt.plot(predictTime, dv, '--k')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
